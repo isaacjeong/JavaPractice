4cff85c56eec3083f18ed2ccc6c32924adba99de
isaac <isaacj1121@gmail.com>
2024-10-09 21:42:24 -0500

    TIL Java Scope

---
+++ w3schools/src/Scope.java
@@ -0,0 +1,10 @@
+public class Scope {
+    public static void main(String[] args) {
+        //code here cannot use x
+        int x = 100;
+
+        //code here can use x
+        System.out.println(x);
+    }
+}
+//ORDER MATTERS
