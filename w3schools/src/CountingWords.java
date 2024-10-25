a509231c8714f48a18dfddb6bc43741dc6887089
isaac <isaacj1121@gmail.com>
2024-10-24 21:52:30 -0500

    TIL Java Calculating the sum of an array and changed code

--- w3schools/src/CountingWords.java
+++ w3schools/src/CountingWords.java
@@ -1,5 +1,7 @@
 public class CountingWords {
-    String words = "One Two Three Four";
-    int countWords = words.split("//s").length;
-    System.out.println(countWords);
+    public static void main(String[] args) {
+        String words = "One Two Three Four";
+        int countWords = words.split("//s").length;
+        System.out.println(countWords);
+    }
 }
