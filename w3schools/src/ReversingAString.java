c3c185e5dd1b94976aa9e2d485dfb05d7263cea4
isaacjeong <Isaacj1121@gmail.com>
2024-11-02 14:06:45 -0500

    TDI Java Reversing A String again.

--- w3schools/src/ReversingAString.java
+++ w3schools/src/ReversingAString.java
@@ -2,11 +2,16 @@ public class ReversingAString {
     public static void main(String[] args) {
         String originalStr = "Hello";
         String reversedStr = "";
-
         for (int i = 0; i < originalStr.length(); i++) {
             reversedStr = originalStr.charAt(i) + reversedStr;
         }
-
         System.out.println("Reversed string: " + reversedStr);
+
+        String org = "Hello";
+        String str = "";
+        for (int j = org.length() - 1; j >= 0; j--) {
+            str = str + org.charAt(j);
+        }
+        System.out.println(str);
     }
 }
