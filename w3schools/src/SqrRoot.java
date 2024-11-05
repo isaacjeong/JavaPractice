dc4483194d988c15b25f90ecdf271746df866bc4
isaac <isaacj1121@gmail.com>
2024-11-04 18:52:48 -0600

    TIL Java Finding Sqrt, Absolue Value, and Finding Max Value.

---
+++ w3schools/src/SqrRoot.java
@@ -0,0 +1,11 @@
+public class SqrRoot {
+    public static void main(String[] args) {
+        double a = Math.sqrt(64);
+        double b = Math.abs(64);
+        double c = Math.max(64, 16);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
