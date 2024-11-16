92ed02ea33b53b51a74d944939738511d45b4013
isaac <isaacj1121@gmail.com>
2024-11-15 18:55:11 -0600

    TIL Creating And Adding Things In HashSet

---
+++ w3schools/src/CreateHashSet.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+public class CreateHashSet {
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
