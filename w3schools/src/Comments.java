75cee9109c6020823dee7647ef580f55b7d2619f
isaac <isaacj1121@gmail.com>
2024-11-18 20:55:42 -0600

    TIL About Java Comments

---
+++ w3schools/src/Comments.java
@@ -0,0 +1,9 @@
+public class Comments {
+    public static void main(String[] args) {
+        //Comment before line
+        System.out.println("Hello"); //Comment after code
+        /*
+        Multi-Line comment
+         */
+    }
+}
