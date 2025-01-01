acee48755358dcb93086e0b2dc5fa4ed7c29401f
isaacj <Isaacj1121@gmail.com>
2024-12-31 19:37:23 -0600

    TIL Java Constructors With Parameters

---
+++ w3schools/src/ConstructorWithParameters.java
@@ -0,0 +1,16 @@
+import javax.net.ssl.CertPathTrustManagerParameters;
+
+public class ConstructorWithParameters {
+    int modelYear;
+    String modelName;
+
+    public ConstructorWithParameters(int year, String name) {
+        modelYear = year;
+        modelName = name;
+    }
+
+    public static void main(String[] args) {
+        ConstructorWithParameters myCar = new ConstructorWithParameters(1969, "Mustang");
+        System.out.println(myCar.modelYear + " " + myCar.modelName);
+    }
+}
