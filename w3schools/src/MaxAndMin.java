8bfaf299fbae1337696d6e116f49a46dbdf6991a
isaacj <Isaacj1121@gmail.com>
2025-03-09 20:27:34 -0500

    TIL Java Max and Min

--- w3schools/src/MaxAndMin.java
+++ w3schools/src/MaxAndMin.java
@@ -1,6 +1,6 @@
 public class MaxAndMin {
     public static void main(String[] args) {
-        System.out.println(Math.max(5, 10));
-        System.out.println(Math.min(5, 10));
+        System.out.println(Math.max(1, 10));
+        System.out.println(Math.min(1, 10));
     }
 }
