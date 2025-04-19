b6f2807ea1b4d6ee1caad7e88a900141a51be753
isaacjeong <Isaacj1121@gmail.com>
2025-04-19 10:56:03 -0500

    Today I reviewed Fibonnaci Sequence(Code is a little wrong)

---
+++ w3schools/src/Fibonnaci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Fibonnaci {
+    public static void main(String[] args) {
+
+        long startNumber = 1;
+        long startNumberTwo = 1;
+
+        Scanner firstFib = new Scanner(System.in);
+        System.out.println("Enter your nth number: ");
+        long fibScanner = firstFib.nextInt() - 2;
+
+        long sum = 0;
+        for(long i = 0; i < fibScanner; i++){
+            sum = startNumber + startNumberTwo;
+            startNumber = startNumberTwo;
+            startNumberTwo = sum;
+        }
+        System.out.println(sum);
+    }
+}
